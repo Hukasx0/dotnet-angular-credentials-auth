ece753114aa9b1f91c372a3f6e7586b5fe852a83
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-30 12:00:33 +0100

    Prepare Angular files

--- dotnet-angular-credentials-auth.client/src/app/app-routing.module.ts
+++ dotnet-angular-credentials-auth.client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dotnet-angular-credentials-auth.client/src/proxy.conf.js
+++ dotnet-angular-credentials-auth.client/src/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api/Auth",
     ],
     target: "https://localhost:7256",
     secure: false
