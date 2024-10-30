9044b8cadc47e2456de5b7dc03a2ef812d6c05d4
Hubert Kasperek <hubertkasp13@gmail.com>
2024-10-30 09:38:12 +0100

    Add User model and login and register DTOs

---
+++ dotnet-angular-credentials-auth.Server/Models/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_angular_credentials_auth.Server.Models
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ dotnet-angular-credentials-auth.Server/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_angular_credentials_auth.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
